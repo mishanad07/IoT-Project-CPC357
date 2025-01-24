6ae4ea6f7024d3b4e9d5e58ea394a337fbf9ee0a
mishanad07 <amishanadhira@student.usm.my>
2025-01-24 12:21:18 +0800

    Add files via upload

---
+++ vonesetting.h
@@ -0,0 +1,18 @@
+#if !defined (VONESETTING_H)
+#define VONESETTING_H
+#else
+#error Multiple includes of vonesetting.h
+#endif
+
+#define WIFI_SSID "HUAWEI-B310-730F"          //Replace this with YOUR WiFi SSID
+#define WIFI_PASSWORD "047BHMTHG2B"  //Replace this with YOUR WiFi Password
+
+#define MQTT_SERVER "mqtt.v-one.my"
+#define MQTT_PORT 8883
+#define MQTT_USERNAME "doxDtILEgI1lJmpt"  //Replace this with the Access Token of YOUR gateway
+#define MQTT_PASSWORD " "
+
+#define GATEWAYID "551043bb-e2a5-4da4-98d2-bc98b6f9bfd8"  //Replace this with the gatewayID of your gateway
+#define INTERVAL 1000 //1S
+#define INTERVAL2 500 //0.5S
+
